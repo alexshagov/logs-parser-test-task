debef3c9b3d344a1f7f6bef02bf8a5a4922862e3
Alexander Shagov <alexander.shgv@gmail.com>
2019-04-18 09:50:39 +0300

    Add uniq page views spec

---
+++ spec/statistics/unique_page_views_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require_relative '../../lib/app.rb'
+
+RSpec.describe Statistics::UniquePageViews do
+  subject { described_class.new(log_path) }
+
+  let(:log_path) do
+    File.join(File.expand_path("../fixtures", __dir__), 'webserver.log')
+  end
+
+  let(:expected_statistics) do
+    {
+      "/about" => 21,
+      "/about/2" => 22,
+      "/contact" => 23,
+      "/help_page/1" => 23,
+      "/home" => 23,
+      "/index" => 23,
+    }
+  end
+
+  it 'prints statistics' do
+    expect(subject.print!).to include expected_statistics
+  end
+end
